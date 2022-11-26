5f9baa6396e0ad0eaf3647aaebcdb58ff10698fd
Zoe Daniels <zoefaithdaniels094>
2022-11-26 11:37:41 +0200

    Calculator

---
+++ index.js
@@ -0,0 +1,25 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operations')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const equalButton = document.querySelector('[data-equals]')
+
+clear() {
+
+}
+delete(){
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
